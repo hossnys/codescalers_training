e86613d2210f3b9d3ba7dfb4bda665991e0a4c88
hossnys <hossnys@codescalers.com>
2018-07-16 11:37:51 +0200

    refining code

--- python101/.vscode/phonbook.py
+++ python101/.vscode/phonbook.py
@@ -2,23 +2,23 @@ addressbook = {"samir": "01003295601", "peter": "01003333222"}
 
 
 def addUser():
-    x= input("please enter your name")
-    y= input("please enter youe phone")
+    x= input("please enter your name: ")
+    y= input("please enter youe phone: ")
     addressbook[x] = y
     print('user added')
 
 
 def delUser():
-    x= input("please enter the name you want to delete")
+    x= input("please enter the name you want to delete: ")
     del addressbook[x]
 
 
 def listUsers():
-    print("here is the address book contents")
+    print("here is the address book contents:\n")
     print(addressbook.keys(), addressbook.values())
 
 def searchUser():
-    x = input("pleas enter the user or phone you search about")
+    x = input("pleas enter the user or phone you search about: ")
     if x in addressbook:
         print("your name is there: "+ x +": " +addressbook[x])
     else:
